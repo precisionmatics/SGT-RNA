1af941db3d11b27d1eb4d9e969366ee1347dc22e
Stalin <precision.stalin@gmail.com>
2026-07-08 16:09:53 +0530

    Remove remaining PDFL refs in step17_gnina_docking.py

--- scripts/step17_gnina_docking.py
+++ scripts/step17_gnina_docking.py
@@ -295,12 +295,12 @@ log.info("\n" + "="*70)
 log.info("HYBRID MODEL EVALUATION (step17 features)")
 log.info("="*70)
 
-# Step11 feature slices from step15 (step15 = step11 + shape(8) + lig_pdfl(3600))
+# Step11 feature slices from step15 (step15 = step11 + shape(8) + lig_sgt(3600))
 # step11 layout within step15:
-#   PDFL[0:36000], Morgan[36000:38048], NucComp[38048:38058], Physico[38058:38064]
+#   SGT[0:36000], Morgan[36000:38048], NucComp[38048:38058], Physico[38058:38064]
 #   RNA-FM[38064:38704], SS[38704:38712], kmer[38712:38796], MACCS[38796:38963]
 #   Iface4[38963:42563], Iface6[42563:46163], Iface8[46163:49763]
-#   Shape[49763:49771], LigPDFL[49771:53371]
+#   Shape[49763:49771], LigSGT[49771:53371]
 # step17 adds 2 more at the end: [53371:53373]
 
 # Use full X17 for Ridge model
